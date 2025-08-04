f06176b939099ea121554c12bc928890e8b70bd1
Takinol1 <103071304+Takinol1@users.noreply.github.com>
2025-08-04 15:54:41 -0500

    Switch back to Browser router and added error handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import News from "./Pages/News";
 import Finder from "./Pages/Finder";
 
-const routes = [
-  { path: "/", element: <Home /> },
-  { path: "/news", element: <News /> },
-  { path: "/news-search", element: <Finder /> },
-];
-
-const router = createBrowserRouter(routes);
-
 function App() {
   return (
-    <HashRouter>
-      <RouterProvider router={router} />
-    </HashRouter>
+    <Router basename="/informative-news">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/news-search" element={<Finder />} />
+      </Routes>
+    </Router>
   );
 }
 
