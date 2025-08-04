ddbeefd88c7a88213160db91f43b880d5be6d3fc
Takinol1 <103071304+Takinol1@users.noreply.github.com>
2025-08-04 15:27:18 -0500

    Fix React Router basename for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,20 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home";
 import News from "./Pages/News";
 import Finder from "./Pages/Finder";
-const routes = createBrowserRouter([
+
+const routes = [
   { path: "/", element: <Home /> },
   { path: "/news", element: <News /> },
   { path: "/news-search", element: <Finder /> },
-]);
+];
+
+// Pass basename option here:
+const router = createBrowserRouter(routes, {
+  basename: "/informative-news",
+});
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
