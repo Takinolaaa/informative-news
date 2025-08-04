75f98f0971063c4fa5b16acb03b7b9e76c76f9a3
Takinol1 <103071304+Takinol1@users.noreply.github.com>
2025-08-04 16:09:24 -0500

    Fix routing and update deployment settings

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,12 +2,12 @@ export default function Navbar() {
   return (
     <nav className="flex  bg-black font-mono text-white p-4 justify-between">
       {" "}
-      <a className="text-white text-bold font-mono " href="/">
+      <a className="text-white text-bold font-mono " href="/informative-news/">
         Informative News
       </a>{" "}
       <div className="flex gap-2">
-        <a href="/news">Trending News</a>
-        <a href="/news-search">Find Articles</a>
+        <a href="/informative-news/news">Trending News</a>
+        <a href="/informative-news/news-search">Find Articles</a>
       </div>
     </nav>
   );
